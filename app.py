324d3cf0cbe64bc385b32cd30aaec4dda4dbcdf3
Ahmad Alhashemi <ahmad@ahmadh.com>
2013-12-14 11:49:48 -0500

    removed file system specific code and relied on Flask and Python features to create and consume temporary file handlers

--- app.py
+++ app.py
@@ -1,36 +1,33 @@
 import os
-from flask import Flask, request, redirect, url_for, send_from_directory, render_template
-from werkzeug import secure_filename
+from flask import Flask, request, redirect, url_for, send_file, render_template
 from zipmirror import zipmirror
-
-UPLOAD_FOLDER = 'tmp/mirrored'
-ALLOWED_EXTENSIONS = set(('zip', ))
+from tempfile import TemporaryFile
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.debug = True
 app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
 
 def allowed_file(filename):
   return '.' in filename and \
-    filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    filename.rsplit('.', 1)[1] == 'zip'
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
   if request.method == 'POST':
     uploaded_file = request.files['archive']
     if uploaded_file and allowed_file(uploaded_file.filename):
-      root, ext = os.path.splitext(secure_filename(uploaded_file.filename))
-      attachment_filename = root + '-rtl' + ext
-      save_path = os.path.join(
-        app.config['UPLOAD_FOLDER'],
-        attachment_filename)
-      mirrored_file = open(save_path, 'w')
+      mirrored_file = TemporaryFile()
       zipmirror(uploaded_file, mirrored_file)
-      return send_from_directory(
-        app.config['UPLOAD_FOLDER'],
-        attachment_filename,
+      mirrored_file.seek(0)
+
+      root, ext = os.path.splitext(uploaded_file.filename)
+      attachment_filename = root + '-rtl' + ext
+      
+      return send_file(
+        filename_or_fp = mirrored_file,
         mimetype = 'application/zip',
         as_attachment = True,
-        attachment_filename = attachment_filename)
+        attachment_filename = attachment_filename,
+        add_etags = False)
   else:
     return render_template('index.html')
