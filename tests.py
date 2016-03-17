869e8d3386ad1b37682619eb02117fb094aade5b
Ahmad Alhashemi <ahmad@ahmadh.com>
2016-03-17 12:42:26 -0400

    Test whole CSS file mirroring.

--- tests.py
+++ tests.py
@@ -8,5 +8,10 @@ class MirrorLibTest(unittest.TestCase):
   def test_css_property_in_comment(self):
     self.assertEqual(cssmirror('/* p { text-align: left; } */'), '/* p { text-align: left; } */')
 
+  def test_css_file(self):
+    with open('fixtures/1-input.css', 'r') as i:
+      with open('fixtures/1-output.css', 'r') as o:
+        self.assertEqual(cssmirror(i.read()), o.read())
+
 if __name__ == '__main__':
   unittest.main()
