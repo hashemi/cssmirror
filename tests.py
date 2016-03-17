ba4678968f9d9e2b9841b0e14228f195f7847b7a
Ahmad Alhashemi <ahmad@ahmadh.com>
2016-03-17 19:26:53 -0400

    Tests for gif, jpeg and tiff files

--- tests.py
+++ tests.py
@@ -5,6 +5,14 @@ from mirrorlib.zipmirror import zipmirror
 from mirrorlib.mirrorselect import mirrorselect
 from io import BytesIO
 
+def mirror_image(image, times):
+  result = image
+  for x in range(times):
+    temp = BytesIO()
+    imgmirror(result, temp)
+    result = temp
+  return result
+
 class MirrorLibTest(unittest.TestCase):
   def test_select_inert_file(self):
     self.assertIsNone(mirrorselect('example.txt'))
@@ -39,21 +47,39 @@ class MirrorLibTest(unittest.TestCase):
 
   def test_mirror_png_file(self):
     with open('fixtures/test.png', 'rb') as i:
-      mirrored_once = BytesIO()
-      imgmirror(i, mirrored_once)
-      mirrored_twice = BytesIO()
-      imgmirror(mirrored_once, mirrored_twice)
-      self.assertNotEqual(mirrored_once.getvalue(), mirrored_twice.getvalue())
+      mirrored_once = mirror_image(i, times=1).getvalue()
+      mirrored_twice = mirror_image(i, times=2).getvalue()
+      self.assertNotEqual(mirrored_once, mirrored_twice)
 
   def test_double_mirror_png_file(self):
     with open('fixtures/test.png', 'rb') as i:
-      mirrored_once = BytesIO()
-      imgmirror(i, mirrored_once)
-      mirrored_twice = BytesIO()
-      imgmirror(mirrored_once, mirrored_twice)
-      mirrored_thrice = BytesIO()
-      imgmirror(mirrored_twice, mirrored_thrice)
-      self.assertEqual(mirrored_once.getvalue(), mirrored_thrice.getvalue())
+      mirrored_once = mirror_image(i, times=1).getvalue()
+      mirrored_thrice = mirror_image(i, times=3).getvalue()
+      self.assertEqual(mirrored_once, mirrored_thrice)
+
+  def test_mirror_gif_file(self):
+    with open('fixtures/test.gif', 'rb') as i:
+      mirrored_once = mirror_image(i, times=1).getvalue()
+      mirrored_twice = mirror_image(i, times=2).getvalue()
+      self.assertNotEqual(mirrored_once, mirrored_twice)
+
+  def test_double_mirror_gif_file(self):
+    with open('fixtures/test.gif', 'rb') as i:
+      mirrored_once = mirror_image(i, times=1).getvalue()
+      mirrored_thrice = mirror_image(i, times=3).getvalue()
+      self.assertEqual(mirrored_once, mirrored_thrice)
+
+  def test_mirror_jpeg_file(self):
+    with open('fixtures/test.jpg', 'rb') as i:
+      mirrored_once = mirror_image(i, times=1).getvalue()
+      mirrored_twice = mirror_image(i, times=2).getvalue()
+      self.assertNotEqual(mirrored_once, mirrored_twice)
+
+  def test_mirror_tiff_file(self):
+    with open('fixtures/test.tiff', 'rb') as i:
+      mirrored_once = mirror_image(i, times=1).getvalue()
+      mirrored_twice = mirror_image(i, times=2).getvalue()
+      self.assertNotEqual(mirrored_once, mirrored_twice)
 
   def test_zip_with_mac_metadata(self):
     with open('fixtures/mac-metadata-test.zip', 'rb') as i:
