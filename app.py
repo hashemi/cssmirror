981a39c2f2801a260b7c6edaae9cf438537360b7
Ahmad Alhashemi <ahmad@ahmadh.com>
2016-03-16 23:34:50 -0400

    Better error messages for invalid uploads.

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from tempfile import TemporaryFile
 from time import time
 from werkzeug.datastructures import Headers
 try:
-    from werkzeug.wsgi import wrap_file
+  from werkzeug.wsgi import wrap_file
 except ImportError:
-    from werkzeug.utils import wrap_file
+  from werkzeug.utils import wrap_file
 
 app = Flask(__name__)
 app.debug = True
@@ -21,25 +21,31 @@ def allowed_file(filename):
 def upload_file():
   if request.method == 'POST':
     uploaded_file = request.files['archive']
-    if uploaded_file and allowed_file(uploaded_file.filename):
-      mirrored_file = TemporaryFile()
-      zipmirror(uploaded_file, mirrored_file)
-      mirrored_file.seek(0)
 
-      root, ext = os.path.splitext(uploaded_file.filename)
-      attachment_filename = root + '-rtl' + ext
+    if not uploaded_file:
+      return ('No uploaded file.', 400, [])
 
-      headers = Headers()
-      headers.add('Content-Disposition', 'attachment',
+    if not allowed_file(uploaded_file.filename):
+      return ('Invalid file. Only zip files are accepted.', 400, [])
+
+    mirrored_file = TemporaryFile()
+    zipmirror(uploaded_file, mirrored_file)
+    mirrored_file.seek(0)
+
+    root, ext = os.path.splitext(uploaded_file.filename)
+    attachment_filename = root + '-rtl' + ext
+
+    headers = Headers()
+    headers.add('Content-Disposition', 'attachment',
                       filename=attachment_filename)
-      data = wrap_file(request.environ, mirrored_file)
-      rv = app.response_class(data, mimetype='application/zip',
+    data = wrap_file(request.environ, mirrored_file)
+    rv = app.response_class(data, mimetype='application/zip',
                       headers=headers, direct_passthrough=True)
-      rv.cache_control.public = True
-      cache_timeout = app.get_send_file_max_age(attachment_filename)
-      if cache_timeout is not None:
+    rv.cache_control.public = True
+    cache_timeout = app.get_send_file_max_age(attachment_filename)
+    if cache_timeout is not None:
           rv.cache_control.max_age = cache_timeout
           rv.expires = int(time() + cache_timeout)
-      return rv
+    return rv
   else:
     return render_template('index.html')
